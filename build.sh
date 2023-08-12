70cad399008df6bd81870577620aaaf21e6263e8
Pavel Pertsev <morhetz@gmail.com>
2023-08-12 11:39:17 +0200

    add mono icon

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 set -ev
 
 iconutil --convert icns --output Jugoslavica.bundle/Contents/Resources/Jugoslavica\ -\ Flag.icns Flag.iconset
+iconutil --convert icns --output Jugoslavica.bundle/Contents/Resources/Jugoslavica\ -\ Mono.icns Mono.iconset
